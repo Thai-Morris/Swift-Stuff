a61bc53cc28f0dab11d18362af8ac7d9e14f4c29
Thai Morris <30935442+Thai-Morris@users.noreply.github.com>
2019-01-28 21:24:12 -0500

    this is my progress from my text adventure
    I need to commit here daily as part of my challenge to do 100 days of code.

---
+++ text-adventure.swift
@@ -0,0 +1,65 @@
+//: A SpriteKit based Playground
+
+import Foundation
+
+// This is player class with health and a name
+class Player {
+    var name: String
+    var health: Double
+    
+    init(name: String, health: Double) {
+        self.name = name
+        self.health = health
+    }
+    
+    func getName() -> String {
+        return "Your name is \(name)"
+    }
+    func isnotdead(health: Double) -> Double {
+    if health > 0 {
+    
+    }
+    else {
+    print("You're dead bruh")
+    }
+        return health
+    }
+    
+}
+
+struct Enemy {
+    var damage: Double
+    var health: Double
+    var description: String
+    
+}
+
+
+//enemies
+var Terrorist = Enemy(damage: 67, health: 100, description: "A gun wielding terrorist abushes you screaming 'allah ackbar!'")
+var LargeSpider = Enemy(damage: 76, health: 100, description: "A large spider crawled down!")
+
+class Actions {
+    
+    //Attack Function
+    
+}
+
+//rooms
+struct WorldMap {
+    
+}
+
+
+
+// option directions
+enum Direction {
+    case North
+    case East
+    case West
+    case South
+}
+
+
+
+
